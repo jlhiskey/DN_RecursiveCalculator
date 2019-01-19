1de19ea8b4ab7756bd60c3815b33ce7cbe1d2e18
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-18 16:25:35 -0800

    Well I definently forgot to commit at all...

---
+++ DN_RecursiveCalculator/DN_RecursiveCalculator/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace DN_RecursiveCalculator
+{
+    
+    public class Program
+    {
+        public enum CalcType { add, subtract, multiply, divide, useRoot, useExponent };
+
+        static void Main(string[] args)
+        {
+            Calculator(4, CalcType.add, 2);
+            Calculator(4, CalcType.subtract, 2);
+            Calculator(4, CalcType.multiply, 2);
+            Calculator(2, CalcType.divide, 2);
+            Calculator(4, CalcType.useRoot, 2);
+            Calculator(4, CalcType.useExponent, 2);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Takes 2 integers and a calculation type and returns a console of the answer.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="calcType"></param>
+        /// <param name="b"></param>
+        static void Calculator(decimal a, CalcType calcType, decimal b )
+        {
+            
+            switch (calcType)
+            {
+                case CalcType.add:
+                    Console.WriteLine($"{a} + {b} = {Add(a, b)}");
+                    break;
+                case CalcType.subtract:
+                    Console.WriteLine($"{a} - {b} = {Subtract(a, b)}");
+                    break;
+                case CalcType.multiply:
+                    Console.WriteLine($"{a} * {b} = {Multiply(a, b)}");
+                    break;
+                case CalcType.divide:
+                    Console.WriteLine($"{a} / {b} = {Divide(a, b)}");
+                    break;
+                case CalcType.useRoot:
+                    Console.WriteLine($"{b} root of {a} = {NthRoot(a, b)}");
+                    break;
+                case CalcType.useExponent:
+                    Console.WriteLine($"{a} ^ {b} = {Exponent(a, b)}");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// a + b. b must be positive.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>integer</returns>
+        public static decimal Add(decimal a, decimal b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+
+            return Add(a + 1, b - 1);
+        }
+
+        /// <summary>
+        /// a - b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>integer</returns>
+        public static decimal Subtract(decimal a, decimal b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+
+            return Subtract(a - 1, b - 1);
+        }
+
+        /// <summary>
+        /// a * b. b must be positive.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>integer</returns>
+        public static decimal Multiply(decimal a, decimal b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+
+            return a + Multiply(a , b - 1);
+        }
+
+        /// <summary>
+        /// a / b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>integer</returns>
+        public static decimal Divide(decimal a, decimal b)
+        {
+            decimal counter = 0;
+
+            decimal HelperDivide(decimal aHelper, decimal bHelper, decimal counterHelper)
+            {
+                if (bHelper == 0)
+                {
+                    return 0;
+                }
+                if (aHelper < bHelper)
+                {
+                    return 0;
+                }
+                else
+                {
+                    counter = counter + 1;
+                    return ((aHelper - bHelper) - HelperDivide(aHelper - bHelper, bHelper, counter));
+                }
+            }
+            HelperDivide(a, b, counter);
+            return counter;
+            
+        }
+
+        /// <summary>
+        /// b root of a. Only returns whole integer.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>integer</returns>
+        public static decimal NthRoot(decimal a, decimal b)
+        {
+            decimal counter = 0;
+            decimal HelperRoot( decimal aHelper, decimal bHelper, decimal counterHelper)
+            {
+                if(aHelper == 0)
+                {
+                    return counterHelper;
+                }
+                else
+                {
+
+                    return HelperRoot(aHelper - bHelper, bHelper, counterHelper + 1);
+                }
+            }
+            counter = HelperRoot(a, b, counter);
+            return counter;
+        }
+
+        /// <summary>
+        /// a ^ b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>integer</returns>
+        public static decimal Exponent(decimal a, decimal b )
+        {
+            b = b - 1;
+            decimal HelperExponent(decimal aHelper, decimal bHelper)
+            {
+                if (bHelper == 0)
+                {
+                    return aHelper;
+                }
+                else
+                {
+                    return HelperExponent(aHelper * aHelper, bHelper - 1);
+                }
+            }
+              a = HelperExponent(a, b);
+            return a;
+        }
+    }
+}
---
+++ DN_RecursiveCalculator/DN_RecursiveCalculatorTestSuite/UnitTest1.cs
@@ -0,0 +1,111 @@
+using System;
+using Xunit;
+using DN_RecursiveCalculator;
+
+namespace DN_RecursiveCalculatorTestSuite
+{
+    public class AddTestSuite
+    {
+        [Fact]
+        public void TestingAddMethodOne()
+        {
+            Assert.Equal(4, Program.Add(2, 2));
+        }
+
+        [Fact]
+        public void TestingAddMethodTwo()
+        {
+            Assert.Equal(3, Program.Add(1, 2));
+        }
+
+        [Fact]
+        public void TestingAddMethodThree()
+        {
+            Assert.Equal(9, Program.Add(2, 7));
+        }
+    }
+
+    public class SubtractTestSuite
+    {
+        [Fact]
+        public void TestingSubtractMethodOne()
+        {
+            Assert.Equal(0, Program.Subtract(2, 2));
+        }
+
+        [Fact]
+        public void TestingSubtractMethodTwo()
+        {
+            Assert.Equal(-1, Program.Subtract(1, 2));
+        }
+
+        [Fact]
+        public void TestingSubtractMethodThree()
+        {
+            Assert.Equal(-5, Program.Subtract(2, 7));
+        }
+    }
+
+    public class MultiplyTestSuite
+    {
+        [Fact]
+        public void TestingMultiplyMethodOne()
+        {
+            Assert.Equal(4, Program.Multiply(2, 2));
+        }
+
+        [Fact]
+        public void TestingMultiplyMethodTwo()
+        {
+            Assert.Equal(2, Program.Multiply(1, 2));
+        }
+
+        [Fact]
+        public void TestingMultiplyMethodThree()
+        {
+            Assert.Equal(-7, Program.Multiply(-1, 7));
+        }
+    }
+
+    public class DivideTestSuite
+    {
+        [Fact]
+        public void TestingDivideMethodOne()
+        {
+            Assert.Equal(1, Program.Divide(2, 2));
+        }
+
+        [Fact]
+        public void TestingDivideMethodTwo()
+        {
+            Assert.Equal(10, Program.Divide(20, 2));
+        }
+
+        [Fact]
+        public void TestingDivideMethodThree()
+        {
+            Assert.Equal(3, Program.Divide(15, 5));
+        }
+    }
+
+    public class DivideTestSuite
+    {
+        [Fact]
+        public void TestingDivideMethodOne()
+        {
+            Assert.Equal(1, Program.Divide(2, 2));
+        }
+
+        [Fact]
+        public void TestingDivideMethodTwo()
+        {
+            Assert.Equal(10, Program.Divide(20, 2));
+        }
+
+        [Fact]
+        public void TestingDivideMethodThree()
+        {
+            Assert.Equal(3, Program.Divide(15, 5));
+        }
+    }
+}
